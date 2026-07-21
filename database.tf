7f5571d935c3ca5949c5831c63e0d7e2782c6021
charlene <satikey@gmail.com>
2026-07-21 14:32:38 +1000

    Add inbound rules for rds-sg, move datatebase related variables to one single file:variables-db.tf and upgrade terraform aws provider version to 6.55.0

--- database.tf
+++ database.tf
@@ -25,3 +25,55 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_app_private_a_traffic" {
+  security_group_id = aws_security_group.rds_sg.id
+  ip_protocol       = "tcp"
+  cidr_ipv4         = var.private_subnet_app_cidrs[0]
+  from_port         = 5432
+  to_port           = 5432
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_app_private_b_traffic" {
+  security_group_id = aws_security_group.rds_sg.id
+  ip_protocol       = "tcp"
+  cidr_ipv4         = var.private_subnet_app_cidrs[1]
+  from_port         = 5432
+  to_port           = 5432
+}
+
+resource "aws_db_instance" "vaultpay" {
+  # RDS instance general settings
+  identifier     = "vaultpay-postgres-instance"
+  instance_class = "db.t4g.micro"
+  engine         = "postgres"
+  engine_version = "18"
+
+  # Database settings
+  db_name = var.db_name
+
+  # Credentials
+  username                    = var.db_username
+  manage_master_user_password = true
+
+  # Storage
+  storage_type      = "gp3"
+  allocated_storage = 20
+  storage_encrypted = true
+
+  # Network & Access
+  publicly_accessible    = false
+  db_subnet_group_name   = aws_db_subnet_group.vaultpay.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  # Environment-aware settings
+  multi_az                = var.multi_az
+  backup_retention_period = var.backup_retention_period
+  deletion_protection     = var.deletion_protection
+  skip_final_snapshot     = var.skip_final_snapshot
+
+  tags = {
+    Name    = "${var.project_name}-postgres"
+    Project = var.project_name
+  }
+}
